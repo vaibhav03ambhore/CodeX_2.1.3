f3aa31c259c1a2a4199d61c664e4537ed54e0999
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-02-18 03:03:16 +0530

    filter UI added for categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@
 import Navbar from './Components/Navbar'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import BrowserBid from './pages/BrowserBid'
+// import BrowserBid from './pages/BrowserBid'
+
 import Footer from './Components/Footer';
 import Login from './pages/Login'
 import Register from './pages/Register';
 import Role from './pages/Role';
+import Browse from './pages/Browse';
 
 function App() {
 
@@ -15,7 +17,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/browserbid" element={<BrowserBid />} />
+        <Route path="/browserbid" element={<Browse/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/role" element={<Role />} />
