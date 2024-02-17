c478139d84cd24d3da5096eee4a423be6bb18ff1
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-02-18 03:08:52 +0530

    Pages Added in Website

--- frontend/src/pages/Role.jsx
+++ frontend/src/pages/Role.jsx
@@ -3,28 +3,21 @@ import { ImUsers } from "react-icons/im";
 import { ImUserTie } from "react-icons/im";
 const Role = () => {
     return (
-        <div className='h-[100vh] flex'>
-            <div className="p-[20px] w-full sm:flex  items-center">
-                <div className="left md:w-[50%]">
-                    <div className='flex-col m-7 text-5xl text-center text-orange-600 font-serif p-9'>
+        <div className='h-[100vh] '>
+            <div className="p-[20px] w-full  sm:flex grid grid-cols-2 gap-4">
+                <div className="left md:w-[50%] flex justify-center p-5">
+                    <div className='text-7xl text-center text-orange-600 font-serif'>
                         <ImUsers />
-                        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                User Role
-                            </span>
-                        </button>
                     </div>
+                        <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">User Role</button>
                 </div>
-                <div className="right md:w-[50%] ">
-                    <div className="flex-col m-7 text-5xl text-center text-orange-600 font-serif p-9">
+                <div className="right md:w-[50%] flex justify-center p-5">
+                    <div className="text-7xl  text-center text-orange-600 font-serif ">
                         <ImUserTie />
-                        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                Admin Role
-                            </span>
-                        </button>
                     </div>
+                        <button type="button" class="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Admin Role</button>
                 </div>
+                <div />
             </div>
         </div>
     )
