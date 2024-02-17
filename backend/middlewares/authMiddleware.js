5d0743a143e58341c19538e4dadf6e6a63c98ffe
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-02-18 00:06:31 +0530

    autherization code removed

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 import Jwt  from "jsonwebtoken";
-import User from "../models/userModel.js";
 import asyncHandler from "./asyncHandler.js";
+import Organizer from "../models/userModel.js";
 
 const authenticate = asyncHandler(async (req, res, next) => {
     let token;
@@ -10,7 +10,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
     if(token){
         try{
             const decoded=Jwt.verify(token,process.env.JWT_SECRET);
-            req.user=await User.findById(decoded.userId).select("-password");
+            req.organizer=await Organizer.findById(decoded.userId).select("-password");
             next();
         }catch(error){
             res.status(401);
@@ -23,16 +23,4 @@ const authenticate = asyncHandler(async (req, res, next) => {
     }
 });
 
-//check if user is admin or not
-
-const authorizeAdmin=asyncHandler(async(req,res,next)=>{
-    if(req.user && req.user.isAdmin){
-        next();
-    }
-    else{
-        res.status(401);
-        throw new Error("Not authorized as an admin");
-    }
-});
-
-export {authenticate,authorizeAdmin};
+export {authenticate};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,11 @@ import express from "express";
 import {createOrganizer,loginOrganizer,logoutCurrentOrganizer,getAllOrganizers} from '../controllers/userController.js';
 
 
-import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
+import { authenticate} from "../middlewares/authMiddleware.js";
 
 const router=express.Router();
 
-router.route('/').post(createOrganizer).get(authenticate,authorizeAdmin,getAllOrganizers);
+router.route('/').post(createOrganizer).get(authenticate,getAllOrganizers);
 router.post('/login',loginOrganizer);
 router.post('/logout',logoutCurrentOrganizer);
 
