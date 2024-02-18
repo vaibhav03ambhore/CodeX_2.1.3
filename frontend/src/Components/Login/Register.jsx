d464939b2e93200027800cfd2cbe050aaf4adc87
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-02-18 10:00:44 +0530

    Ui Improving

---
+++ frontend/src/Components/Login/Register.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Register = () => {
+    return (
+        <div className=''>
+            <section className="bg-gray-50 dark:bg-gray-900 flex grid justify-items-center">
+                <div className="py-4 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+                    <div>
+                        <div className="w-full lg:max-w-xl p-6 space-y-4 sm:p-4 bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                            <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+                                Register For Auction Katta
+                            </h2>
+                            <form className="mt-8 space-y-6" action="#">
+                                <div>
+                                    <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                                    <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required />
+                                </div>
+                                <div>
+                                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="yourname@gmail.com" required />
+                                </div>
+                                <div>
+                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                </div>
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" required />
+                                    </div>
+                                    <div className="ms-3 text-sm">
+                                        <label for="remember" className="font-medium text-gray-500 dark:text-gray-400">Remember this device</label>
+                                    </div>
+                                </div>
+                                <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+                                <div className="text-sm font-medium text-gray-900 dark:text-white">
+                                    Already registered ? <Link to={'/login'}><a className="text-blue-600 hover:underline dark:text-blue-500">Click Here Login</a></Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default Register
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import CarouselPage from '../Components/CarouselPage'
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
-        <div className='h-[100vh]'>
+        <div className='h-[100vh] bg-gray-50'>
             <div className="p-[40px] w-full sm:flex gap-4 items-center">
-                <div className="left md:w-[60%] rounded-3xl">
-                    <div className='flex-col m-7 text-3xl text-center text-orange-600 font-serif p-9'>
-                        <div className=''>
-                        <h2>Discover Here</h2>
-                        <h3>The World Largest Bidding Platform</h3>
-                        </div>
-                    <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                            Read More
-                        </span>
-                    </button>
+                <div className="left md:w-[50%] rounded-3xl">
+                    <div className='justify-center text-center'>
+                        <h1 className='text-4xl	p-3'>Discover</h1>
+                        <h2 className='text-5xl text-red-600 font-bold p-2'>The World</h2>
+                        <h3 className='text-6xl'>Largest Bidding Platform</h3>
+                        <p className='text-1xl font-medium m-4'>Experience the thrill of auctioning your cherished art, collectibles, and real estate on our dynamic online platform. Unleash the power of bidding to unlock the true value of your treasures in a seamless and engaging digital marketplace.</p>
+                        <Link to={'/readmore'}>
+                        <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Read More</button>
+                        </Link>
                     </div>
                 </div>
                 <div className="right md:w-[50%] py-9">
