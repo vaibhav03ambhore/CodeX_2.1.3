fc60fa869da2252ad3d47fe22cd0b1189b6e0f11
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-02-18 11:03:27 +0530

    D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './Components/Login/Login'
 import Register from './Components/Login/Register';
 import Role from './Components/Login/Role';
 import ReadMore from './Components/ReadMore';
+import Chatbot from 'react-chatbot-kit';
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/role" element={<Role />} />
         <Route path="/readmore" element={<ReadMore />} />
+        <Route path="/chatbot" element={<Chatbot/>} />
       </Routes>
       <Footer />
     </div>
