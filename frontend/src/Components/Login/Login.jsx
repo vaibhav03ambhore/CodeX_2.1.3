970144796bde0304130e7e211579f8f3f7e70d2a
SAJanGAIKwad <sajan.gaikwad21@pccoepune.org>
2024-02-18 18:23:38 +0530

    Bidding page Done

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Login = () => {
     return (
         <div>
-            <section className="bg-gray-50  flex items-center justify-center">
+            <section className="bg-gray-50  flex items-center justify-center pt-[80px]">
                 <div className="py-4 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
                     <div>
                         <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl">
--- frontend/src/pages/Bidding.jsx
+++ frontend/src/pages/Bidding.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import CarouselPage from '../Components/CarouselPage'
+import { Link } from 'react-router-dom'
 const Bidding = () => {
-  return (
-    <div>Bidding</div>
-  )
+    return (
+        <section className='pt-[80px]'>
+            <div className='h-[80vh] bg-gray-50 relative'>
+                <div className="p-[40px] w-full sm:flex gap-4 items-center">
+                    <div className="left md:w-[50%] rounded-3xl">
+                        <div className='justify-center text-center font-[italic]'>
+                            <h1 className='text-4xl	p-3'>Discover</h1>
+                            <h2 className='text-5xl text-red-600 font-bold p-2'>The World</h2>
+                            <h3 className='text-6xl'>Largest Bidding Platform</h3>
+                            <p className='text-1xl font-medium m-4'>Experience the thrill of auctioning your cherished art, collectibles, and real estate on our dynamic online platform. Unleash the power of bidding to unlock the true value of your treasures in a seamless and engaging digital marketplace.</p>
+                            <Link to={'/readmore'}>
+                                <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Read More</button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="right md:w-[50%] py-4">
+                        <CarouselPage className='border-rounded'/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Bidding
