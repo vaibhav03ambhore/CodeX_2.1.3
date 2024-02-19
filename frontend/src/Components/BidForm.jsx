0a165e3bd7fdcec3240bfb05900812e37f9de13e
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-02-19 09:23:53 +0530

    form date sending to backend need autharization

--- frontend/src/Components/BidForm.jsx
+++ frontend/src/Components/BidForm.jsx
@@ -1,12 +1,76 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-const BidForm = () => {
-  // const [selectedFile, setSelectedFile] = useState(null);
+import { useState } from 'react';
+import axios from 'axios';
 
-  // const handleFileChange=(event)=>{
-  //   setSelectedFile(event.target.files[0]);
-  //   // console.log(event.target.files)
-  // }
+const BidForm = () => {
+  
+    const [formData, setFormData] = useState({
+        name: '',
+        category: '',
+        detail: '',
+        price: '',
+        rules: '',
+        fixedIncrement: ''
+
+    });
+
+
+      const [selectedFile, setSelectedFile] = useState(null);
+  
+      const handleFileChange=(event)=>{
+        console.log('file changed')
+        setSelectedFile(event.target.files[0]);
+        // console.log(event.target.files)
+        console.log('selected file is : ',selectedFile)
+      }
+  
+      const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+     
+      const handleSubmit = async(event) => {
+        event.preventDefault();   //prevent to submit the form traditional way
+  
+        const formDataToSend = new FormData();
+  
+        Object.entries(formData).forEach(([key, value]) => {
+
+           formDataToSend.append(key, value);
+         
+        });
+  
+        formDataToSend.append('image', selectedFile);
+  
+        console.log("form data to send is ",formDataToSend);
+            for (let pair of formDataToSend.entries()) {
+             console.log(pair[0] + ' - ' + pair[1]);
+        }
+        const emptyFields = Object.values(formData).some(value => value === '');
+        if (emptyFields) {
+            alert('Please fill in all the details.');
+            return; // Exit early if any field is empty
+        }
+         
+        try {
+        
+            const response = await axios.post('http://localhost:3000/api/bid-items/create',formDataToSend,{
+                 headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+    
+      
+          console.log(response.data); 
+    
+        } catch (error) {
+          console.error("Error submitting items:", error);
+          alert(error.response.data.message);
+        }
+          
+         
+    };
+
     return (
         <div className=''>
             <section className="bg-gray-50 flex grid justify-items-center pt-[80px]">
@@ -19,41 +83,33 @@ const BidForm = () => {
                             <form className="mt-8 space-y-6" action="#">
                                 <div>
                                     <label for="name" className="block mb-2 text-sm font-medium text-gray-900">Bid Name</label>
-                                    <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Bid Name" required />
+                                    <input type="name" name="name" id="name" value={formData.name} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Bid Name" required />
                                 </div>
                                 <div>
-                                    <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Bid Category</label>
-                                    <input type="category" name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="bid category" required />
+                                    <label for="category" className="block mb-2 text-sm font-medium text-gray-900">Bid Category</label>
+                                    <input type="category" name="category" id="category" value={formData.category} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="bid category" required />
                                 </div>
                                 <div>
-                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Bid Image</label>
-                                    <input type="file" name="file" id="file"/>
+                                    <label for="file" className="block mb-2 text-sm font-medium text-gray-900">Bid Image</label>
+                                    <input type="file" name="image" onChange={handleFileChange} id="file"/>
                                 </div>
                                 <div>
-                                    <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Bid Details</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                                    <label for="detail" className="block mb-2 text-sm font-medium text-gray-900">Bid Details</label>
+                                    <input type="detail" name="detail" id="detail" placeholder="description/details" value={formData.detail} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                                 </div>
                                 <div>
-                                    <label for="name" className="block mb-2 text-sm font-medium text-gray-900">Base Price</label>
-                                    <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="bid base Price" required />
+                                    <label for="price" className="block mb-2 text-sm font-medium text-gray-900">Base Price</label>
+                                    <input type="number" name="price" id="price" value={formData.price} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="bid base Price" required />
                                 </div>
                                 <div>
-                                    <label for="address" className="block mb-2 text-sm font-medium text-gray-900">Bid Increament</label>
-                                    <input type="bid increament" name="bid increament" id="bid increament" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="your bid increament" required />
+                                    <label for="fixedIncrement" className="block mb-2 text-sm font-medium text-gray-900">Bid Increament</label>
+                                    <input type="number" name="fixedIncrement" id="fixedIncrement" value={formData.fixedIncrement} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="your bid increament" required />
                                 </div>
                                 <div>
                                     <label for="rules" className="block mb-2 text-sm font-medium text-gray-900">Bid Rules</label>
-                                    <input type="address" name="address" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3.5" placeholder="type your rules" required />
+                                    <input type="text" name="rules" id="rules" value={formData.rules} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3.5" placeholder="type your rules" required />
                                 </div>
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
-                                    </div>
-                                    <div className="ms-3 text-sm">
-                                        <label for="remember" className="font-medium text-gray-500 ">Remember this device</label>
-                                    </div>
-                                </div>
-                                <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto">Create Bid</button>
+                                <button type="submit" onClick={handleSubmit} className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto">Create Bid</button>
                                 <div className="text-sm font-medium text-gray-900">
                                     Already registered ? <Link to={'/'}><a className="text-blue-600 hover:underline ">Click Here Login</a></Link>
                                 </div>
